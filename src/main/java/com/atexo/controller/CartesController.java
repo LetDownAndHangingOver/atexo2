a201fe7e363e7105d2f49c5f48a37378f72ae846
Benali <benalimoe@gmail.com>
2021-12-13 19:58:10 +0100

    crossOrigin

--- src/main/java/com/atexo/controller/CartesController.java
+++ src/main/java/com/atexo/controller/CartesController.java
@@ -3,9 +3,9 @@ package com.atexo.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +17,13 @@ import com.atexo.utils.CartesUtils;
 @RestController
 public class CartesController {
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(value="/couleursAleatoires", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<COULEUR> couleurAleatoire(){
 		return CartesUtils.couleurAleatoire();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(value="/dixCartesAleatoires")
 	public Map<String,List<Carte>> dixCartesAleatoires(){
 		Map<String, List<Carte>> mapCartes = new HashMap<String, List<Carte>>();
@@ -32,6 +34,7 @@ public class CartesController {
 		return mapCartes;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(value="/valeursAleatoires")
 	public List<VALEURS> valeursAleatoires(){
 		return CartesUtils.valeursAleatoire();
--- src/test/java/com/atexo/utils/CarteUtilsTest.java
+++ src/test/java/com/atexo/utils/CarteUtilsTest.java
@@ -1,9 +1,12 @@
 package com.atexo.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.Comparator;
 import java.util.List;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -52,5 +55,73 @@ public class CarteUtilsTest {
 		for (int i = 0; i < listeCartes.size(); i++) {
 			assertThat(listeCartes.get(i)).isNotNull();
 		}
+	}
+	
+	@Test()
+	public void triDeCartesCouleurNullValid() {
+		CartesUtils.listeCouleurAleatoire = null;
+		CartesUtils.valeursAleatoire();
+		List<Carte> listeNonTriee = CartesUtils.dixCartesAleatoire();
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			CartesUtils.triDeCartes(listeNonTriee)
+			)
+		.withMessage("Il faut choisir l'ordre de couleurs et de valeurs");
+	}
+	
+	@Test()
+	public void triDeCartesValeursNullValid() {
+		CartesUtils.listeValeurAleatoire = null;
+		CartesUtils.couleurAleatoire();
+		List<Carte> listeNonTriee = CartesUtils.dixCartesAleatoire();
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			CartesUtils.triDeCartes(listeNonTriee)
+			)
+		.withMessage("Il faut choisir l'ordre de couleurs et de valeurs");
+	}
+	
+	@Test()
+	public void triDeCartesCouleurEtValeurNullValid() {
+		CartesUtils.listeCouleurAleatoire = null;
+		CartesUtils.listeValeurAleatoire = null;
+		List<Carte> listeNonTriee = CartesUtils.dixCartesAleatoire();
+		assertThatIllegalArgumentException().isThrownBy(() ->
+			CartesUtils.triDeCartes(listeNonTriee)
+			)
+		.withMessage("Il faut choisir l'ordre de couleurs et de valeurs");
+	}
+	
+	@Test()
+	public void triDeCartesValid() {
+		CartesUtils.couleurAleatoire();
+		CartesUtils.valeursAleatoire();
+		List<Carte> listeNonTriee = CartesUtils.dixCartesAleatoire();
+		
+		List<Carte> listeTriee = CartesUtils.triDeCartes(listeNonTriee);
+		
+		assertThat(listeTriee).isNotNull();
+		
+		assertThat(listeTriee.size()).isEqualTo(10);
+		
+		assertThat(listeTriee).doesNotHaveDuplicates();
+		
+		assertThat(listeTriee).isSortedAccordingTo(new Comparator<Carte>() {
+		@Override
+		public int compare(Carte carte1, Carte carte2) {
+			COULEUR couleur1 = carte1.getCouleur();
+			COULEUR couleur2 = carte2.getCouleur();
+			
+			if(CartesUtils.listeCouleurAleatoire.indexOf(couleur1) < CartesUtils.listeCouleurAleatoire.indexOf(couleur2)) {
+				return -1;
+			} else if(CartesUtils.listeCouleurAleatoire.indexOf(couleur1) > CartesUtils.listeCouleurAleatoire.indexOf(couleur2)) {
+				return 1;
+			}
+			VALEURS valeur1 = carte1.getValeur();
+			VALEURS valeur2 = carte2.getValeur();
+			if(CartesUtils.listeValeurAleatoire.indexOf(valeur1) < CartesUtils.listeValeurAleatoire.indexOf(valeur2)) {
+				return -1;
+			}
+			return 1;
+		}	
+	});
 	}
 }
